c0d636cdb29b28e17bc07570835cda2f51d052a6
Clifton Regis <github email address>
2016-08-27 18:22:44 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,7 @@ Rails.application.routes.draw do
   delete '/signout' => 'sessions#destroy', as: 'logout'
 
   get '/signup', to: 'registrations#new',  as: 'signup'
-<<<<<<< HEAD
-  post '/registrations', to: 'registrations#create'
-=======
+
   post '/signup', to: 'registrations#create'
->>>>>>> 8ba295b0f0de2b303fa96f118783601f90b9f93b
 
 end
