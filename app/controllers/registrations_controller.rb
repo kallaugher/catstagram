899b3bdd3611147b4fa3c5d0bb0f5cc4058df350
kallaugher <alicekallaugher@gmail.com>
2016-08-27 18:53:49 -0400

    update signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < ApplicationController
-  
+
   def new
     @user = User.new
   end
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to photos_path
      else
        render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :photos
+  resources :users
 
   root 'photos#index'
 
