016ad7dd0e7f58e1d291f188c587596aa0ff30f5
kallaugher <alicekallaugher@gmail.com>
2016-08-27 14:52:18 -0400

    add cliff's controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def welcome
+
+  end
+
+  def create
+    # user = User.find_by(params[:user][:name])
+        user = User.find_by(name: params[:name])
+      if user  && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+
+    else
+        "You are logged out"
+
+      render :new
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+end
