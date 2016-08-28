c2c19318fac64985c79ce50bff614bde8e19c0d2
Clifton Regis <github email address>
2016-08-28 15:33:35 -0400

    removed 'as: login' from routes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,8 @@ class Photo < ApplicationRecord
   has_attached_file :image, styles: { medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-	
+	def add_vote
+      self.votes += 1
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
 
   root 'photos#index'
 
-  get '/signin' => 'sessions#new', as: 'signin'
-  post '/signin' => 'sessions#create'
-  delete '/signout' => 'sessions#destroy', as: 'signout'
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  delete '/signout', to: "sessions#destroy", as: 'signout'
 
   get '/signup', to: 'registrations#new',  as: 'signup'
 
   post '/signup', to: 'registrations#create'
 
 end
+# , as: 'signin'
