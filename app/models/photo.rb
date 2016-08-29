1268362232170975b7c9944ab73cc5f78f0ecb8f
kallaugher <alicekallaugher@gmail.com>
2016-08-29 11:18:59 -0400

    add more cats

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
   has_many :tags, through: :photo_tags
   belongs_to :user
 
-  has_attached_file :image, styles: { thumb: "400x400#" }
+  has_attached_file :image, styles: { thumb: "400x400#", medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
